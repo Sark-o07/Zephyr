77dd96511a4cb8a78582c44c25c9accc37eacf15
Sark_oo7 <kosinick01@gmail.com>
2024-06-04 22:50:21 +0100

    feat: fetch related products from DB

--- frontend/src/Components/RelatedProducts/RelatedProducts.jsx
+++ frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './RelatedProducts.css'
-import data_product from '../Assets/data'
 import { Item } from '../Item/Item'
 
-export const RelatedProducts = () => {
+export const RelatedProducts = (props) => {
+  const { category } = props;
+  const [relatedProduct, setRelatedProduct] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:4000/relatedproducts', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({category: category})
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      setRelatedProduct(data);
+    });
+  }, []);
+
   return (
     <div className='relatedproducts'>
       <h1>Related Products</h1>
       <hr />
       <div className="relatedproducts-item">
-        {data_product.map((item, i) => {
+        {relatedProduct.map((item, i) => {
           return <Item key={i} id={item.id} image={item.image} name={item.name} old_price={item.old_price} new_price={item.new_price} />
         })}
       </div>
